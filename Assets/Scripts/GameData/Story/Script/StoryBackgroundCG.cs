46d7ac5116c6f2db1b90e750b20feed7fa773bb0
Ddingjae418 <paul.stark15@gmail.com>
2025-08-16 12:06:12 +0900

    add backgorund animation params

--- Assets/Scripts/GameData/Story/Script/StoryBackgroundCG.cs
+++ Assets/Scripts/GameData/Story/Script/StoryBackgroundCG.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Xml.Serialization;
 
 namespace AD.Story
@@ -5,15 +6,62 @@ namespace AD.Story
     [System.Serializable]
     public class StoryBackgroundCG : StoryEntry
     {
-        public StoryBackgroundCG() 
+        /****** Public Members ******/
+
+        public enum AnimationType
         {
-            Type = EntryType.BackgroundCG;
+            Appear,
+            Disappear,
+            Move,
+            Shake,
+            ZoomIn,
+            ZoomOut,
+            AnimtationTypeCount
         }
 
-        [XmlAttribute("Chapter")]
-        public ChapterType Chapter;
+        public enum TargetPositionType
+        {
+            Center = 0,
+            Left,
+            Right,
+            TargetPositionTypeCount
+        }
 
-        [XmlAttribute("ImageName")]
-        public string ImageName;
+        public ChapterType          Chapter             => _chapter;
+        public string               ImageName           => _imageName;
+        public AnimationType        Animation           => _animation;
+        public float                AnimationDuration   => _animationDuration;
+        public TargetPositionType   TargetPosition      => _targetPosition;
+
+        public override bool        IsSavePoint => false;
+        public override EntryType   Type        => EntryType.BackgroundCG;
+
+        public StoryBackgroundCG()
+        {
+            
+        }
+
+        public StoryBackgroundCG(ChapterType chapter, string imageName, AnimationType animation = AnimationType.Appear, float animationDuration = 1.0f, TargetPositionType targetPosition = TargetPositionType.Center)
+        {
+            Debug.Assert(ChapterType.ChapterTypeCount != chapter, "ChapterType must be set to a valid chapter.");
+            Debug.Assert(false == string.IsNullOrEmpty(imageName), "ImageName must not be empty or null.");
+            Debug.Assert(AnimationType.AnimtationTypeCount != animation, "AnimationType must be set to a valid animation type.");
+            Debug.Assert(0 < animationDuration, "AnimationDuration must be greater than zero.");
+
+            _chapter            = chapter;
+            _imageName          = imageName;
+            _animation          = animation;
+            _animationDuration  = animationDuration;
+            _targetPosition     = targetPosition;
+        }
+
+
+        /****** Private Members ******/
+
+        [XmlAttribute("Chapter")]           private     ChapterType         _chapter            = ChapterType.ChapterTypeCount;
+        [XmlAttribute("ImageName")]         private     string              _imageName          = "";
+        [XmlAttribute("Animation")]         private     AnimationType       _animation          = AnimationType.AnimtationTypeCount;
+        [XmlAttribute("AnimationDuration")] private     float               _animationDuration  = 1.0f;
+        [XmlAttribute("TargetPosition")]    private     TargetPositionType  _targetPosition     = TargetPositionType.TargetPositionTypeCount;
     }
 }
