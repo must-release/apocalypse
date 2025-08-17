f3dd143e69f1995e3db49b967d56786b06477005
kangpenguin <rkddnjsrn25@naver.com>
2025-08-18 04:40:58 +0900

    added BackgroundCG Animation

--- Assets/Scripts/Story/StoryPresenter/BackgroundCGHandler.cs
+++ Assets/Scripts/Story/StoryPresenter/BackgroundCGHandler.cs
@@ -32,11 +32,15 @@ namespace AD.Story
             _isCompleted = false;
 
             Sprite bgSprite = _model.GetBackgroundSprite(_currentBackgroundCG.Chapter, _currentBackgroundCG.ImageName);
-            // float duration = _defaultCGAnimationDuration / _currentBackgroundCG.AnimationSpeed;
-
             _backgroundImage.SetSprite(bgSprite);
 
-            await UniTask.Yield();
+            _activeCGAnimationTween = _backgroundImage.PlayAnimation(
+                _currentBackgroundCG.Animation,
+                _currentBackgroundCG.AnimationDuration,
+                _currentBackgroundCG.TargetPosition
+            );
+
+            await _activeCGAnimationTween;
 
             if (_isCompleted)
                 return;
@@ -46,18 +50,19 @@ namespace AD.Story
 
         public void CompleteStoryEntry()
         {
-            // Debug.Assert(null != _currentBackgroundCG, "Current BackgroundCG is null");
-            // Debug.Assert(null != OnStoryEntryComplete, "OnStoryEntryComplete event is not subscribed in BackgroundCGHandler.");
+            Debug.Assert(null != _currentBackgroundCG, "Current BackgroundCG is null");
+            Debug.Assert(null != OnStoryEntryComplete, "OnStoryEntryComplete event is not subscribed in BackgroundCGHandler.");
 
+            // if Animation is Blocker, CANNOT complete this Entry.
             // if (true == _currentBackgroundCG.IsBlockingAnimation)
             //     return;
 
-            // _isCompleted = true;
+            _isCompleted = true;
 
-            // _activeCGAnimationTween?.Complete();
-            // _activeCGAnimationTween = null;
-            // OnStoryEntryComplete.Invoke(this);
-            // _currentBackgroundCG = null;
+            _activeCGAnimationTween?.Complete();
+            _activeCGAnimationTween = null;
+            OnStoryEntryComplete.Invoke(this);
+            _currentBackgroundCG = null;
         }
 
 
--- Assets/Scripts/UI/Controllers/Story/BackgroundImage.cs
+++ Assets/Scripts/UI/Controllers/Story/BackgroundImage.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using AD.Story;
 
 [RequireComponent(typeof(Image))]
 public class BackgroundImage : MonoBehaviour
@@ -12,16 +13,24 @@ public class BackgroundImage : MonoBehaviour
         _image.sprite = sprite;
     }
 
-    public Tween Zoom(float scale, float duration)
+    public Tween PlayAnimation(StoryBackgroundCG.BackgroundAnimationType animationType, float duration, StoryBackgroundCG.BackgroundPositionType targetPositionType)
     {
-        return _image.transform.DOScale(scale, duration);
-    }
-
-    public void SetScale(float scale)
-    {
-        _scaleVector.x = scale;
-        _scaleVector.y = scale;
-        _image.transform.localScale = _scaleVector;
+        switch (animationType)
+        {
+            case StoryBackgroundCG.BackgroundAnimationType.Appear:
+                return Appear(duration);
+            case StoryBackgroundCG.BackgroundAnimationType.Move:
+                Vector2 targetPosition = GetTargetPosition(targetPositionType);
+                return Move(targetPosition, duration);
+            case StoryBackgroundCG.BackgroundAnimationType.ZoomIn:
+                return ZoomIn(duration);
+            case StoryBackgroundCG.BackgroundAnimationType.ZoomOut:
+                return ZoomOut(duration);
+            case StoryBackgroundCG.BackgroundAnimationType.Shake:
+                return Shake(duration, 10f); // Default strength
+            default:
+                return DOTween.Sequence();
+        }
     }
 
 
@@ -30,9 +39,65 @@ public class BackgroundImage : MonoBehaviour
     private Image _image;
     private Vector3 _scaleVector;
 
+    private Vector2 _leftPosition = new Vector2(-500, 0);
+    private Vector2 _rightPosition = new Vector2(500, 0);
+    private Vector2 _centerPosition = new Vector2(0, 0);
+
     private void Awake()
     {
         _image = GetComponent<Image>();
         _scaleVector = Vector3.one;
+    }
+
+    private Tween Zoom(float scale, float duration)
+    {
+        return _image.transform.DOScale(scale, duration);
+    }
+
+    private void SetScale(float scale)
+    {
+        _scaleVector.x = scale;
+        _scaleVector.y = scale;
+        _image.transform.localScale = _scaleVector;
+    }
+
+    private Tween Appear(float duration)
+    {
+        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 0f);
+        return _image.DOFade(1f, duration);
+    }
+
+    private Tween Move(Vector2 targetPosition, float duration)
+    {
+        return _image.transform.DOLocalMove(targetPosition, duration);
+    }
+
+    private Tween ZoomIn(float duration = 1.0f)
+    {
+        return Zoom(1.2f, duration);
+    }
+
+    private Tween ZoomOut(float duration = 1.0f)
+    {
+        return Zoom(0.8f, duration);
+    }
+
+    private Tween Shake(float duration, float strength)
+    {
+        return _image.transform.DOShakePosition(duration, strength);
+    }
+
+    private Vector2 GetTargetPosition(StoryBackgroundCG.BackgroundPositionType targetPositionType)
+    {
+        switch (targetPositionType)
+        {
+            case StoryBackgroundCG.BackgroundPositionType.Left:
+                return _leftPosition;
+            case StoryBackgroundCG.BackgroundPositionType.Right:
+                return _rightPosition;
+            case StoryBackgroundCG.BackgroundPositionType.Center:
+            default:
+                return _centerPosition;
+        }
     }
 }
